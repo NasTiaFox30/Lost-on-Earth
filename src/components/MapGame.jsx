4afb8b837e7d8867226c4bf826ca262563836181
Nastia Bzova <bzovanastia@gmail.com>
2025-09-15 18:41:55 +0200

    created useStates and get .env API key

--- src/components/MapGame.jsx
+++ src/components/MapGame.jsx
@@ -4,3 +4,13 @@ import {
   Marker,
   useLoadScript,
 } from "@react-google-maps/api";
+
+export default function MapGame() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+  });
+
+  const [randomLocation, setRandomLocation] = useState(null);
+  const [userGuess, setUserGuess] = useState(null);
+  const streetViewRef = useRef(null); //container Street View
+}
