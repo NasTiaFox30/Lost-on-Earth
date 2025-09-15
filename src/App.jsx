057a30c012167c48949cba13b281b862a62066dc
Nastia Bzova <bzovanastia@gmail.com>
2025-09-16 01:06:17 +0200

    import and used component Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import MapGame from "./components/MapGame";
+import Logo from "./components/Logo";
 
 export default function App() {
   return (
     <>
+      <Logo/>
       <MapGame />
     </>
   );
