0fd8f2f5c1816898ce8128a17ce0ae2353a0f348
Nastia Bzova <bzovanastia@gmail.com>
2025-09-16 01:57:59 +0200

    Created Copyright component

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,7 @@
+export default function Copyright(){
+    return (
+    <div className="absolute flex flex-col items-start justify-center w-full z-50">
+        <p className="text-xs">Created by: Anastasiia Bzova &copy 2025</p>
+    </div>
+    )    
+}
