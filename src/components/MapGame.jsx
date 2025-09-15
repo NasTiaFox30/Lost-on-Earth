33b3cd2a5c816c200e2208baf098542555447b69
Nastia Bzova <bzovanastia@gmail.com>
2025-09-15 18:43:55 +0200

    formatting

--- src/components/MapGame.jsx
+++ src/components/MapGame.jsx
@@ -6,11 +6,11 @@ import {
 } from "@react-google-maps/api";
 
 export default function MapGame() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-  });
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    });
 
-  const [randomLocation, setRandomLocation] = useState(null);
-  const [userGuess, setUserGuess] = useState(null);
-  const streetViewRef = useRef(null); //container Street View
+    const [randomLocation, setRandomLocation] = useState(null);
+    const [userGuess, setUserGuess] = useState(null);
+    const streetViewRef = useRef(null); //container Street View
 }
