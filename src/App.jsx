a6a33f358f6d77b6995ba10fd52ba8ae35d94153
Nastia Bzova <bzovanastia@gmail.com>
2025-09-16 01:58:09 +0200

    used Copyright component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import MapGame from "./components/MapGame";
 import Logo from "./components/Logo";
+import Copyright from "./components/Copyright";
 
 export default function App() {
   return (
     <>
+      <Copyright/>
       <Logo/>
       <MapGame />
     </>
