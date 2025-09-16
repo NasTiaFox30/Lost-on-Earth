dd5e4ef59eb8daab1d0cdc303df35b39bf379c21
Nastia Bzova <bzovanastia@gmail.com>
2025-09-16 02:25:59 +0200

    mobile adaptation sizes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,14 @@
-export default function Logo(){
+export default function Logo() {
     return (
-    <div className="fixed flex flex-col items-center justify-center w-full z-50">
-        <img src="/public/logo.png" alt=""
-            className="w-30" />
-        <h1 className="bg-green-50 rounded"><strong>Lost-on-Earth</strong></h1>
-    </div>
-    )
+        <div className="fixed flex flex-col items-center justify-center w-full z-50">
+            <img
+                src="/public/logo.png"
+                alt="Lost on Earth Logo"
+                className="w-15 md:w-30"
+            />
+            <h1 className="bg-green-50 rounded text-sm md:text-base">
+                <strong>Lost-on-Earth</strong>
+            </h1>
+        </div>
+    );
 }
